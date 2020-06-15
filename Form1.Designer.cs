5eeb0099edfe0c8d11b16838ef65d1dfebf2c0ca
s18727 <61729768+s18727@users.noreply.github.com>
2020-06-15 22:31:18 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace LinqCwiczenia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ResultsDataGridView = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.WynikTextBox = new System.Windows.Forms.TextBox();
+            this.WartoscLabel = new System.Windows.Forms.Label();
+            this.Przyklad12Button = new System.Windows.Forms.Button();
+            this.Przyklad11Button = new System.Windows.Forms.Button();
+            this.Przyklad10Button = new System.Windows.Forms.Button();
+            this.Przyklad9Button = new System.Windows.Forms.Button();
+            this.Przyklad8Button = new System.Windows.Forms.Button();
+            this.Przyklad7Button = new System.Windows.Forms.Button();
+            this.Przyklad6Button = new System.Windows.Forms.Button();
+            this.Przyklad5Button = new System.Windows.Forms.Button();
+            this.Przyklad4Button = new System.Windows.Forms.Button();
+            this.Przyklad3Button = new System.Windows.Forms.Button();
+            this.Przyklad2Button = new System.Windows.Forms.Button();
+            this.Przyklad1Button = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            
+            ((System.ComponentModel.ISupportInitialize) (this.ResultsDataGridView)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ResultsDataGridView
+            // 
+            this.ResultsDataGridView.ColumnHeadersHeightSizeMode =
+                System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ResultsDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ResultsDataGridView.Location = new System.Drawing.Point(0, 0);
+            this.ResultsDataGridView.Name = "ResultsDataGridView";
+            this.ResultsDataGridView.Size = new System.Drawing.Size(1078, 495);
+            this.ResultsDataGridView.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(Przyklad11Button);
+            this.panel1.Controls.Add(this.Przyklad12Button);
+            this.panel1.Controls.Add(this.WynikTextBox);
+            this.panel1.Controls.Add(this.WartoscLabel);
+            this.panel1.Controls.Add(this.Przyklad10Button);
+            this.panel1.Controls.Add(this.Przyklad9Button);
+            this.panel1.Controls.Add(this.Przyklad8Button);
+            this.panel1.Controls.Add(this.Przyklad7Button);
+            this.panel1.Controls.Add(this.Przyklad6Button);
+            this.panel1.Controls.Add(this.Przyklad5Button);
+            this.panel1.Controls.Add(this.Przyklad4Button);
+            this.panel1.Controls.Add(this.Przyklad3Button);
+            this.panel1.Controls.Add(this.Przyklad2Button);
+            this.panel1.Controls.Add(this.Przyklad1Button);
+            this.panel1.Controls.Add(this.ResetButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 412);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1078, 83);
+            this.panel1.TabIndex = 1;
+            // 
+            // WynikTextBox
+            // 
+            this.WynikTextBox.Location = new System.Drawing.Point(753, 16);
+            this.WynikTextBox.Name = "WynikTextBox";
+            this.WynikTextBox.Size = new System.Drawing.Size(310, 23);
+            this.WynikTextBox.TabIndex = 12;
+            // 
+            // WartoscLabel
+            // 
+            this.WartoscLabel.AutoSize = true;
+            this.WartoscLabel.Location = new System.Drawing.Point(694, 20);
+            this.WartoscLabel.Name = "WartoscLabel";
+            this.WartoscLabel.Size = new System.Drawing.Size(53, 15);
+            this.WartoscLabel.TabIndex = 11;
+            this.WartoscLabel.Text = "Wartość:";
+            // 
+            // Przyklad12Button
+            // 
+            this.Przyklad12Button.Location = new System.Drawing.Point(585, 47);
+            this.Przyklad12Button.Name = "Przyklad12Button";
+            this.Przyklad12Button.Size = new System.Drawing.Size(87, 27);
+            this.Przyklad12Button.TabIndex = 13;
+            this.Przyklad12Button.Text = "Przykład 12";
+            this.Przyklad12Button.UseVisualStyleBackColor = true;
+            this.Przyklad12Button.Click += new System.EventHandler(this.Przyklad12Button_Click);
+            // 
+            // Przyklad11Button
+            // 
+            Przyklad11Button.Location = new System.Drawing.Point(492, 47);
+            Przyklad11Button.Name = "Przyklad11Button";
+            Przyklad11Button.Size = new System.Drawing.Size(87, 27);
+            Przyklad11Button.TabIndex = 14;
+            Przyklad11Button.Text = "Przykład 11";
+            Przyklad11Button.UseVisualStyleBackColor = true;
+            Przyklad11Button.Click += new System.EventHandler(this.Przyklad11Button_Click);
+            // 
+            // Przyklad10Button
+            // 
+            this.Przyklad10Button.Location = new System.Drawing.Point(399, 47);
+            this.Przyklad10Button.Name = "Przyklad10Button";
+            this.Przyklad10Button.Size = new System.Drawing.Size(87, 27);
+            this.Przyklad10Button.TabIndex = 10;
+            this.Przyklad10Button.Text = "Przykład 10";
+            this.Przyklad10Button.UseVisualStyleBackColor = true;
+            this.Przyklad10Button.Click += new System.EventHandler(this.Przyklad10Button_Click);
+            // 
+            // Przyklad9Button
+            // 
+            this.Przyklad9Button.Location = new System.Drawing.Point(306, 47);
+            this.Przyklad9Button.Name = "Przyklad9Button";
+            this.Przyklad9Button.Size = new System.Drawing.Size(87, 27);
+            this.Przyklad9Button.TabIndex = 9;
+            this.Przyklad9Button.Text = "Przykład 9";
+            this.Przyklad9Button.UseVisualStyleBackColor = true;
+            this.Przyklad9Button.Click += new System.EventHandler(this.Przyklad9Button_Click);
+            // 
+            // Przyklad8Button
+            // 
+            this.Przyklad8Button.Location = new System.Drawing.Point(213, 47);
+            this.Przyklad8Button.Name = "Przyklad8Button";
+            this.Przyklad8Button.Size = new System.Drawing.Size(87, 27);
+            this.Przyklad8Button.TabIndex = 8;
+            this.Przyklad8Button.Text = "Przykład 8";
+            this.Przyklad8Button.UseVisualStyleBackColor = true;
+            this.Przyklad8Button.Click += new System.EventHandler(this.Przyklad8Button_Click);
+            // 
+            // Przyklad7Button
+            // 
+            this.Przyklad7Button.Location = new System.Drawing.Point(120, 47);
+            this.Przyklad7Button.Name = "Przyklad7Button";
+            this.Przyklad7Button.Size = new System.Drawing.Size(87, 27);
+            this.Przyklad7Button.TabIndex = 7;
+            this.Przyklad7Button.Text = "Przykład 7";
+            this.Przyklad7Button.UseVisualStyleBackColor = true;
+            this.Przyklad7Button.Click += new System.EventHandler(this.Przyklad7Button_Click);
+            // 
+            // Przyklad6Button
+            // 
+            this.Przyklad6Button.Location = new System.Drawing.Point(585, 12);
+            this.Przyklad6Button.Name = "Przyklad6Button";
+            this.Przyklad6Button.Size = new System.Drawing.Size(87, 27);
+            this.Przyklad6Button.TabIndex = 6;
+            this.Przyklad6Button.Text = "Przykład 6";
+            this.Przyklad6Button.UseVisualStyleBackColor = true;
+            this.Przyklad6Button.Click += new System.EventHandler(this.Przyklad6Button_Click);
+            // 
+            // Przyklad5Button
+            // 
+            this.Przyklad5Button.Location = new System.Drawing.Point(492, 12);
+            this.Przyklad5Button.Name = "Przyklad5Button";
+            this.Przyklad5Button.Size = new System.Drawing.Size(87, 27);
+            this.Przyklad5Button.TabIndex = 5;
+            this.Przyklad5Button.Text = "Przykład 5";
+            this.Przyklad5Button.UseVisualStyleBackColor = true;
+            this.Przyklad5Button.Click += new System.EventHandler(this.Przyklad5Button_Click);
+            // 
+            // Przyklad4Button
+            // 
+            this.Przyklad4Button.Location = new System.Drawing.Point(399, 12);
+            this.Przyklad4Button.Name = "Przyklad4Button";
+            this.Przyklad4Button.Size = new System.Drawing.Size(87, 27);
+            this.Przyklad4Button.TabIndex = 4;
+            this.Przyklad4Button.Text = "Przykład 4";
+            this.Przyklad4Button.UseVisualStyleBackColor = true;
+            this.Przyklad4Button.Click += new System.EventHandler(this.Przyklad4Button_Click);
+            // 
+            // Przyklad3Button
+            // 
+            this.Przyklad3Button.Location = new System.Drawing.Point(306, 12);
+            this.Przyklad3Button.Name = "Przyklad3Button";
+            this.Przyklad3Button.Size = new System.Drawing.Size(87, 27);
+            this.Przyklad3Button.TabIndex = 3;
+            this.Przyklad3Button.Text = "Przykład 3";
+            this.Przyklad3Button.UseVisualStyleBackColor = true;
+            this.Przyklad3Button.Click += new System.EventHandler(this.Przyklad3Button_Click);
+            // 
+            // Przyklad2Button
+            // 
+            this.Przyklad2Button.Location = new System.Drawing.Point(213, 14);
+            this.Przyklad2Button.Name = "Przyklad2Button";
+            this.Przyklad2Button.Size = new System.Drawing.Size(87, 27);
+            this.Przyklad2Button.TabIndex = 2;
+            this.Przyklad2Button.Text = "Przykład 2";
+            this.Przyklad2Button.UseVisualStyleBackColor = true;
+            this.Przyklad2Button.Click += new System.EventHandler(this.Przyklad2Button_Click);
+            // 
+            // Przyklad1Button
+            // 
+            this.Przyklad1Button.Location = new System.Drawing.Point(120, 13);
+            this.Przyklad1Button.Name = "Przyklad1Button";
+            this.Przyklad1Button.Size = new System.Drawing.Size(87, 27);
+            this.Przyklad1Button.TabIndex = 1;
+            this.Przyklad1Button.Text = "Przykład 1";
+            this.Przyklad1Button.UseVisualStyleBackColor = true;
+            this.Przyklad1Button.Click += new System.EventHandler(this.Przyklad1Button_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(14, 14);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(87, 27);
+            this.ResetButton.TabIndex = 0;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1078, 495);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ResultsDataGridView);
+            this.Name = "Form1";
+            this.Text = "Ćwiczenia LINQ";
+            ((System.ComponentModel.ISupportInitialize) (this.ResultsDataGridView)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ResultsDataGridView;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Button Przyklad1Button;
+        private System.Windows.Forms.Button Przyklad2Button;
+        private System.Windows.Forms.Button Przyklad3Button;
+        private System.Windows.Forms.Button Przyklad4Button;
+        private System.Windows.Forms.Button Przyklad5Button;
+        private System.Windows.Forms.Button Przyklad6Button;
+        private System.Windows.Forms.Button Przyklad7Button;
+        private System.Windows.Forms.Button Przyklad8Button;
+        private System.Windows.Forms.Button Przyklad9Button;
+        private System.Windows.Forms.Button Przyklad10Button;
+        private System.Windows.Forms.Button Przyklad11Button;
+        private System.Windows.Forms.Button Przyklad12Button;
+        private System.Windows.Forms.Label WartoscLabel;
+        private System.Windows.Forms.TextBox WynikTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,350 @@
+﻿using LinqCwiczenia.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqCwiczenia
+{
+    public partial class Form1 : Form
+    {
+        public IEnumerable<Emp> Emps { get; set; }
+        public IEnumerable<Dept> Depts { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            var empsCol = new List<Emp>();
+            var deptsCol = new List<Dept>();
+
+            #region Load depts
+            var d1 = new Dept
+            {
+                Deptno = 1,
+                Dname = "Research",
+                Loc = "Warsaw"
+            };
+
+            var d2 = new Dept
+            {
+                Deptno = 2,
+                Dname = "Human Resources",
+                Loc = "New York"
+            };
+
+            var d3 = new Dept
+            {
+                Deptno = 3,
+                Dname = "IT",
+                Loc = "Los Angeles"
+            };
+
+            deptsCol.Add(d1);
+            deptsCol.Add(d2);
+            deptsCol.Add(d3);
+            Depts = deptsCol;
+            #endregion
+
+            #region Load emps
+            var e1 = new Emp
+            {
+                Deptno = 1,
+                Empno = 1,
+                Ename = "Jan Kowalski",
+                HireDate = DateTime.Now.AddMonths(-5),
+                Job = "Backend programmer",
+                Mgr = null,
+                Salary = 2000
+            };
+
+            var e2 = new Emp
+            {
+                Deptno = 1,
+                Empno = 20,
+                Ename = "Anna Malewska",
+                HireDate = DateTime.Now.AddMonths(-7),
+                Job = "Frontend programmer",
+                Mgr = e1,
+                Salary = 4000
+            };
+
+            var e3 = new Emp
+            {
+                Deptno = 1,
+                Empno = 2,
+                Ename = "Marcin Korewski",
+                HireDate = DateTime.Now.AddMonths(-3),
+                Job = "Frontend programmer",
+                Mgr = null,
+                Salary = 5000
+            };
+
+            var e4 = new Emp
+            {
+                Deptno = 2,
+                Empno = 3,
+                Ename = "Paweł Latowski",
+                HireDate = DateTime.Now.AddMonths(-2),
+                Job = "Frontend programmer",
+                Mgr = e2,
+                Salary = 5500
+            };
+
+            var e5 = new Emp
+            {
+                Deptno = 2,
+                Empno = 4,
+                Ename = "Michał Kowalski",
+                HireDate = DateTime.Now.AddMonths(-2),
+                Job = "Backend programmer",
+                Mgr = e2,
+                Salary = 5500
+            };
+
+            var e6 = new Emp
+            {
+                Deptno = 2,
+                Empno = 5,
+                Ename = "Katarzyna Malewska",
+                HireDate = DateTime.Now.AddMonths(-3),
+                Job = "Manager",
+                Mgr = null,
+                Salary = 8000
+            };
+
+            var e7 = new Emp
+            {
+                Deptno = null,
+                Empno = 6,
+                Ename = "Andrzej Kwiatkowski",
+                HireDate = DateTime.Now.AddMonths(-3),
+                Job = "System administrator",
+                Mgr = null,
+                Salary = 7500
+            };
+
+            var e8 = new Emp
+            {
+                Deptno = 2,
+                Empno = 7,
+                Ename = "Marcin Polewski",
+                HireDate = DateTime.Now.AddMonths(-3),
+                Job = "Mobile developer",
+                Mgr = null,
+                Salary = 4000
+            };
+
+            var e9 = new Emp
+            {
+                Deptno = 2,
+                Empno = 8,
+                Ename = "Władysław Torzewski",
+                HireDate = DateTime.Now.AddMonths(-9),
+                Job = "CTO",
+                Mgr = null,
+                Salary = 12000
+            };
+
+            var e10 = new Emp
+            {
+                Deptno = 2,
+                Empno = 9,
+                Ename = "Andrzej Dalewski",
+                HireDate = DateTime.Now.AddMonths(-4),
+                Job = "Database administrator",
+                Mgr = null,
+                Salary = 9000
+            };
+
+            empsCol.Add(e1);
+            empsCol.Add(e2);
+            empsCol.Add(e3);
+            empsCol.Add(e4);
+            empsCol.Add(e5);
+            empsCol.Add(e6);
+            empsCol.Add(e7);
+            empsCol.Add(e8);
+            empsCol.Add(e9);
+            empsCol.Add(e10);
+            Emps = empsCol;
+            ResultsDataGridView.DataSource = Emps;
+
+            #endregion
+
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+     
+
+        
+        private void Przyklad1Button_Click(object sender, EventArgs e)
+        {
+          
+            var result = Emps.Where(emp => emp.Job == "Backend programmer").ToList();
+
+            ResultsDataGridView.DataSource = result;
+        }
+
+        private void Przyklad2Button_Click(object sender, EventArgs e)
+        {
+            var res = (from emp in Emps
+                       join dept in Depts on emp.Deptno equals dept.Deptno
+                       where emp.Job == "Frontend programmer" && emp.Salary > 1000
+                       orderby emp.Ename descending
+                       select emp).ToList();
+            
+         
+            var result = Emps
+                .Where(emp => emp.Job == "Frontend programmer" && emp.Salary > 1000)
+                .OrderByDescending(emp => emp.Ename)
+                .ToList();
+            
+            ResultsDataGridView.DataSource = result;
+        }
+
+       
+        private void Przyklad3Button_Click(object sender, EventArgs e)
+        {
+           
+            
+            var result = Emps.Max(emp => emp.Salary);
+            
+            WynikTextBox.Text = result + "";
+        }
+
+      
+        private void Przyklad4Button_Click(object sender, EventArgs e)
+        {
+        
+            var result = Emps
+                .Where(emp => emp.Salary ==
+                              Emps.Max(emp2 => emp2.Salary))
+                .ToList();
+            
+            ResultsDataGridView.DataSource = result;
+        }
+
+       
+        private void Przyklad5Button_Click(object sender, EventArgs e)
+        {
+            var result = Emps
+                .Select(emp => new
+                {
+                    Nazwisko = emp.Ename,
+                    Praca = emp.Job
+                })
+                .ToList();
+            
+            ResultsDataGridView.DataSource = result;
+        }
+
+    
+        private void Przyklad6Button_Click(object sender, EventArgs e)
+        {
+            var result = Emps
+                .Join(Depts, emp => emp.Deptno, dept => dept.Deptno, (emp, dept) => new
+                {
+                    emp.Ename,
+                    emp.Job,
+                    dept.Dname
+                })
+                .ToList();
+            
+            ResultsDataGridView.DataSource = result;
+        }
+
+        private void Przyklad7Button_Click(object sender, EventArgs e)
+        {
+            var result = Emps
+                .GroupBy(emp => emp.Job)
+                .Select(emp => new
+                {
+                    Praca = emp.Key,
+                    LiczbaPracownikow = emp.Count()
+                })
+                .ToList();
+            
+            ResultsDataGridView.DataSource = result;
+        }
+
+    
+        private void Przyklad8Button_Click(object sender, EventArgs e)
+        {
+            
+            if (Emps.Any(emp => emp.Job == "Backend programmer"))
+            {
+                WynikTextBox.Text = "Backend programmer istnieje";
+            }
+        }
+
+   
+        private void Przyklad9Button_Click(object sender, EventArgs e)
+        {
+
+            var result = Emps
+                .Where(emp => emp.Job == "Frontend programmer")
+                .OrderByDescending(emp => emp.HireDate)
+                .FirstOrDefault();
+            
+            ResultsDataGridView.DataSource = new List<Emp> {result};
+        }
+
+     
+        private void Przyklad10Button_Click(object sender, EventArgs e)
+        {
+            var result = Emps
+                .Select(emp => new
+                {
+                    emp.Ename,
+                    emp.Job,
+                    emp.HireDate
+                })
+                .Union(Emps
+                    .Select(temp => new
+                    {
+                        Ename = "Brak wartości",
+                        Job = (string) null,
+                        HireDate = (DateTime?) null
+                    }))
+                    .ToList();
+            
+            ResultsDataGridView.DataSource = result;
+        }
+        
+     
+        private void Przyklad11Button_Click(object sender, EventArgs e)
+        {
+            var result = Emps
+                .Aggregate((emp, next) => emp.Salary > next.Salary ? emp : next);
+            
+            ResultsDataGridView.DataSource = new List<Emp> {result};
+        }
+     
+        private void Przyklad12Button_Click(object sender, EventArgs e)
+        {
+            var result = Emps
+                .SelectMany(emp => Depts, (emp, dept) => new
+                {
+                    emp,
+                    dept
+                })
+                .ToList();
+            ResultsDataGridView.DataSource = result;
+        }
+    }
+}
